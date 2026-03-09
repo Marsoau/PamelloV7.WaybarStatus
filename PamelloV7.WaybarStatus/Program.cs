1f8cc5b766ee14b3fbd845a0b880645a2b37759a
Marsoau <koteika5063@gmail.com>
2026-03-10 00:02:50 +0200

    Initial Creation

---
+++ PamelloV7.WaybarStatus/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace PamelloV7.WaybarStatus;
+
+public class Program
+{
+    public static async Task Main(string[] args) {
+        for (var i = 0;; i++) {
+            Console.WriteLine($"Hello {i}");
+            await Task.Delay(1000);
+        }
+    }
+}
