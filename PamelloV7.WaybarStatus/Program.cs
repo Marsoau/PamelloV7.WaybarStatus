e683028ffab4437eb3e9adcbbdc136ab02aa5e2b
Marsoau <koteika5063@gmail.com>
2026-03-10 00:52:00 +0200

    Connection & info display

--- PamelloV7.WaybarStatus/Program.cs
+++ PamelloV7.WaybarStatus/Program.cs
@@ -1,11 +1,106 @@
-﻿namespace PamelloV7.WaybarStatus;
+﻿using System.Text;
+using PamelloV7.Core.Audio;
+using PamelloV7.Wrapper;
+using PamelloV7.Wrapper.Entities;
+
+namespace PamelloV7.WaybarStatus;
 
 public class Program
 {
+    public static readonly PamelloClient Client;
+    
+    public static RemotePlayer? Player;
+    public static RemoteSong? Song;
+    
+    public static string Url;
+    public static Guid Token;
+    
+    static Program() {
+        Client = new PamelloClient();
+    }
+    
     public static async Task Main(string[] args) {
-        for (var i = 0;; i++) {
-            Console.WriteLine($"Hello {i}");
-            await Task.Delay(1000);
+        var configPath = args.Length > 0 ? args[0] : "pamellostatus.config";
+        
+        if (!File.Exists(configPath)) {
+            Console.WriteLine("No config file found");
+            return;
         }
+        
+        var parts = (await File.ReadAllTextAsync(configPath)).Split("\n");
+        
+        if (parts.Length == 0 || string.IsNullOrWhiteSpace(parts[0])) {
+            Console.WriteLine("Url and token not found in config file");
+            return;
+        }
+        if (parts.Length == 1 || string.IsNullOrWhiteSpace(parts[1])) {
+            Console.WriteLine("Token not found in config file");
+            return;
+        }
+        
+        Url = parts[0];
+        Token = Guid.Parse(parts[1]);
+        
+        Console.WriteLine($"{Url} : {Token}");
+        
+        await Update();
+        await Start();
+        
+        await Task.Delay(-1);
+    }
+
+    public static async Task Start() {
+        _ = Client.StartConnectionAttemptsAsync(Url);
+
+        Client.OnFailedAttempt += (x, attempt) => {
+            //Console.WriteLine($"Failed attempt {attempt}: {x}");
+        };
+        
+        Client.OnConnected += () => {
+            _ = Update();
+            _ = Client.AuthorizeAsync(Token);
+        };
+        Client.OnDisconnected += () => {
+            _ = Update();
+            _ = Client.StartConnectionAttemptsAsync(Url);
+        };
+        
+        Client.OnAuthorized += () => _ = Update();
+        Client.OnUnauthorized += () => _ = Update();
+
+        Client.Events.Watch(Update, () => [
+            Client.User, Player, Song
+        ]);
+    }
+
+    public static async Task Update() {
+        if (!Client.Signal.IsConnected) {
+            Console.WriteLine("Disconnected");
+            return;
+        }
+        if (!Client.Signal.IsAuthorized) {
+            Console.WriteLine("Unauthorized");
+            return;
+        }
+        
+        Player = await Client.PEQL.GetSingleAsync<RemotePlayer>(Client.RequiredUser.SelectedPlayerId ?? 0);
+        Song = await Client.PEQL.GetSingleAsync<RemoteSong>(Player?.Queue.CurrentSongId ?? 0);
+        
+        var sb = new StringBuilder();
+
+        if (Song is not null) {
+            sb.Append(Song);
+            if (Player is not null) {
+                sb.Insert(0, $"[{Player.Queue.Position + 1}/{Player.Queue.Entries.Count()}] | ");
+                sb.Append(" | ").Append(new AudioTime(Player.Queue.CurrentSongTimePassed).ToShortString());
+                sb.Append(" / ").Append(new AudioTime(Player.Queue.CurrentSongTimeTotal).ToShortString());
+                if (Player.IsPaused) sb.Append(" (Paused)");
+            }
+        }
+        else {
+            sb.Append(Player?.ToString() ?? "No player");
+        }
+
+        Console.WriteLine(sb.ToString());
     }
 }
